23a1bf1663ccf75774ef43936f5a98c1878b5297
GustavoPitbull <83386216+Houseint@users.noreply.github.com>
2025-04-02 10:08:17 -0300

    App.js

--- ativ/CalculadoraIMC/App.js
+++ ativ/CalculadoraIMC/App.js
@@ -1,21 +1,23 @@
-import { StyleSheet, View } from "react-native";
-import Title from "./components/title";
-import FormIMC from "./components/FormIMC";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from 'react';
 
-export default function App(){
+export default function App() {
     return (
         <View style={styles.container}>
-            <Title/>
-            <FormIMC/>
+
+            <Text> Gustavo </Text>
+
+            <StatusBar style="auto" />
         </View>
-    )
+    );
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        padding: 16,
         backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-})
+});
